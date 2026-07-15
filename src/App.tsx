136ee593f7e2e10788832dd6ce619563c56029c4
Zain Ul Hassan <zainprog56@gmail.com>
2026-07-15 09:26:35 +0800

    first comment

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-950 text-white">
+      <h1 className="text-5xl font-bold text-blue-400">
+        My Portfolio
+      </h1>
+    </div>
+  )
+}
+
+export default App
